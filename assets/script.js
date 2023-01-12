b08a539328fba975b20875fbcfd327c7c7ae2bfa
Crothos <s.g.totten@gmail.com>
2023-01-11 21:08:19 -0600

    Current conditions now show on page after submit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 var form = document.getElementById("cityform");
 
+var currentCity;
+var currentWeather;
+var currentTemp;
+var currentWind;
+var currentHumidity;
 
 
-//var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=benbrook&appid=b4bddf8a88e63b2b5f41a2b614c276d6&units=imperial";
+//5-day Forecast = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=b4bddf8a88e63b2b5f41a2b614c276d6&units=imperial"
 
 form.addEventListener('submit', citySubmit);
 
@@ -15,12 +20,30 @@ form.addEventListener('submit', citySubmit);
 
  function getApi() {
     var city = localStorage.getItem('city');
-     fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=b4bddf8a88e63b2b5f41a2b614c276d6&units=imperial")
+     fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=b4bddf8a88e63b2b5f41a2b614c276d6&units=imperial")
      .then(function (response) {
        return response.json();
      })
      .then(function (data) {
-       console.log(data);
-     });
-};
+        currentCity = data.name;
+        currentWeather = data.weather[0].main;
+        currentTemp = data.main.temp;
+        currentWind = data.wind.speed;
+        currentHumidity = data.main.humidity;
+
+        renderCurrentConditions();
+    
+})
+function renderCurrentConditions() { 
+    var elem = document.createElement("img");
+    elem.setAttribute("src", "http://openweathermap.org/img/wn/10d@2x.png");
+    elem.setAttribute("alt", "Weather Icon");
+    document.getElementById("weather-list").append(elem);
+    document.getElementById("currentCity").innerText = "City: " + currentCity;
+    document.getElementById("currentCond").innerText = "Conditions: " + currentWeather;
+    document.getElementById("currentTemp").innerText = "Temperature: " + currentTemp +"F";
+    document.getElementById("currentWind").innerText = "Wind Speed: " + currentWind +"mph";
+    document.getElementById("currentHumid").innerText = "Humidity: " + currentHumidity + "%";
+}
+}
 
