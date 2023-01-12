3971d722da89bc11fbf7e16216ef8d8cac3c410e
Crothos <s.g.totten@gmail.com>
2023-01-11 19:09:02 -0600

    API call on form submit added

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,26 @@
-var city;
-var apiUrl = `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=b4bddf8a88e63b2b5f41a2b614c276d6&units=imperial`;
+var form = document.getElementById("cityform");
+
+
+
+//var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=benbrook&appid=b4bddf8a88e63b2b5f41a2b614c276d6&units=imperial";
+
+form.addEventListener('submit', citySubmit);
+
+ function citySubmit(event){
+    event.preventDefault();
+    var city = document.getElementById("cityname").value;
+    localStorage.setItem('city', city);
+     getApi();
+}
+
+ function getApi() {
+    var city = localStorage.getItem('city');
+     fetch("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=b4bddf8a88e63b2b5f41a2b614c276d6&units=imperial")
+     .then(function (response) {
+       return response.json();
+     })
+     .then(function (data) {
+       console.log(data);
+     });
+};
+
